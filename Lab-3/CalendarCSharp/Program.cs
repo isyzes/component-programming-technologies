3a1357f5d4e7c9b60d9dd43880213326335ec46a
Dmitry <isyzes@gmail.com>
2026-05-22 13:59:53 +0300

    Add task 4 for lab 4

---
+++ Lab-3/CalendarCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalendarCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MSACAL.Calendar calendar = new MSACAL.Calendar();
+
+            PrintCalendarInfo(calendar);
+
+            calendar.Day = 31;
+            calendar.BackColor = 54;
+
+            PrintCalendarInfo(calendar);
+        }
+
+        static void PrintCalendarInfo(MSACAL.Calendar calendar)
+        {
+            Console.WriteLine($"[Календарь] День: {calendar.Day}, Цвет фона: {calendar.BackColor}");
+        }
+    }
+}
